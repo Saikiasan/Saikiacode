a14f11ab9c513651804e1d3a2f87116809c6c7f4
Ankit Saikia <112230331+Saikiasan@users.noreply.github.com>
2022-12-22 19:12:30 +0530

    new commit

---
+++ app/src/main/java/com/ankit/saikiacode/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ankit.saikiacode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
